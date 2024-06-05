1c916d19ffcdc4d977ed6a7b53835d9495193761
44oo <168866215+44oo@users.noreply.github.com>
2024-06-05 03:19:49 +0300

    Update superspeed.lua

--- superspeed.lua
+++ superspeed.lua
@@ -1,6 +1,5 @@
 getgenv().safedie = true
-
-game:GetService("RunService").stepped:connect(function()
+ame:GetService("RunService").stepped:connect(function()
             if safedie == true then
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0,0,-1.6)
 
