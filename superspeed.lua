9c06c9ddc14b63583126bb36fb9ed4f9fc95bde6
44oo <168866215+44oo@users.noreply.github.com>
2024-06-05 03:11:28 +0300

    Update superspeed.lua

--- superspeed.lua
+++ superspeed.lua
@@ -2,10 +2,7 @@ getgenv().Sky = true
 game:GetService("RunService").heartbeat:Connect(function()
 if getgenv().Sky ~= true then 
 
-delay(0, function()
-	pcall(function()
-	game:GetService("RunService").stepped:connect(function()
-			if safedie == true then
+			
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0,0,-1.6)
 
 		end
