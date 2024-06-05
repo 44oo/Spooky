4ae8a34e8885310d8700629d99aa24c03348bd60
44oo <168866215+44oo@users.noreply.github.com>
2024-06-05 03:14:30 +0300

    Update superspeed.lua

--- superspeed.lua
+++ superspeed.lua
@@ -1,11 +1,29 @@
-getgenv().Sky = true
-game:GetService("RunService").heartbeat:Connect(function()
-if getgenv().Sky ~= true then 
+getgenv().safedie = true
 
 			
+mouse = game.Players.LocalPlayer:GetMouse()
+mouse.KeyDown:Connect(function(key)
+    if key == "c" then
+safedie = true
+end
+end)
+
+mouse.KeyUp:Connect(function(key)
+    if key == "c" then
+safedie = false
+end
+end)
+
+
+
+    delay(0, function()
+    pcall(function()
+    game:GetService("RunService").stepped:connect(function()
+            if safedie == true then
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0,0,-1.6)
 
-		end
-		end)
-		end)
+        end
+        end)
+        end)
+end)
 end)
